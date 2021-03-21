6e404cd158c9ff60eb652512ba103ae6bef16cb5
jonayeid <jonayeidk@gmail.com>
2021-03-21 18:07:55 +0600

    details design updated

--- lib/home/article.dart
+++ lib/home/article.dart
@@ -59,8 +59,8 @@ class _ArticleState extends State<Article> {
         title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text("Flutter"),
-            Text(" Blog",style: TextStyle(
+            Text("News"),
+            Text(" Title",style: TextStyle(
                 color: Colors.blue
             ),)
           ],
@@ -76,67 +76,62 @@ class _ArticleState extends State<Article> {
       )
           : SingleChildScrollView(
         padding: EdgeInsets.symmetric(horizontal: 16),
-        child: Container(
-          child: Column(
-            children: <Widget>[
-              // blogs
-              Container(
-                margin: EdgeInsets.only(top: 16),
-                child: Column(
-                  children: <Widget>[
-                    SizedBox(height: 8,),
-                    Text(
-                      postModel.postTitle,
-                      textAlign:TextAlign.left,
-                      style: TextStyle(
-                        fontSize: 18,
-                        color: Colors.black87,
-                        fontWeight: FontWeight.w500,
-
-                      ),),
-                    ClipRRect(
-                      borderRadius: BorderRadius.circular(6),
-                      child: Image.network(postModel.imageLink),
-                    ),
-                    SizedBox(height: 8,),
-                    Html(
-                      data: postModel.postContent,
-                      //Optional parameters:
-                      customImageRenders: {
-                        networkSourceMatcher(domains: ["flutter.dev"]):
-                            (context, attributes, element) {
-                          return FlutterLogo(size: 36);
-                        },
-                        networkSourceMatcher(domains: ["mydomain.com"]): networkImageRender(
-                          headers: {"Custom-Header": "some-value"},
-                          altWidget: (alt) => Text(alt),
-                          loadingWidget: () => Text("Loading..."),
-                        ),
-                        // On relative paths starting with /wiki, prefix with a base url
-                            (attr, _) => attr["src"] != null && attr["src"].startsWith("/wiki"):
-                        networkImageRender(
-                            mapUrl: (url) => "https://upload.wikimedia.org" + url),
-                        // Custom placeholder image for broken links
-                        networkSourceMatcher(): networkImageRender(altWidget: (_) => FlutterLogo()),
-                      },
-                      onLinkTap: (url) {
-                        print("Opening $url...");
-                      },
-                      onImageTap: (src) {
-                        print(src);
-                      },
-                      onImageError: (exception, stackTrace) {
-                        print(exception);
-                      },
-                    ),
-                    // Text(postModel.postContent,style: TextStyle(
-                    //     color: Colors.black54
-                    // ),)
-                  ],
-                ),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(height: 24),
+            Text(
+              postModel.postTitle,
+              textAlign:TextAlign.start,
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.black87,
+                fontWeight: FontWeight.w500,
+              ),),
+            SizedBox( height: 8,),
+            Text(
+              postModel.reporterName,
+              textAlign:TextAlign.start,
+              style: TextStyle(
+                  color: Colors.black54
               ),
-            ],
-          ),
+            ),
+            ClipRRect(
+              borderRadius: BorderRadius.circular(6),
+              child: Image.network(postModel.imageLink),
+            ),
+            SizedBox(height: 8,),
+            Html(
+              data: postModel.postContent,
+              //Optional parameters:
+              customImageRenders: {
+                networkSourceMatcher(domains: ["flutter.dev"]):
+                    (context, attributes, element) {
+                  return FlutterLogo(size: 36);
+                },
+                networkSourceMatcher(domains: ["mydomain.com"]): networkImageRender(
+                  headers: {"Custom-Header": "some-value"},
+                  altWidget: (alt) => Text(alt),
+                  loadingWidget: () => Text("Loading..."),
+                ),
+                // On relative paths starting with /wiki, prefix with a base url
+                    (attr, _) => attr["src"] != null && attr["src"].startsWith("/wiki"):
+                networkImageRender(
+                    mapUrl: (url) => "https://upload.wikimedia.org" + url),
+                // Custom placeholder image for broken links
+                networkSourceMatcher(): networkImageRender(altWidget: (_) => FlutterLogo()),
+              },
+              onLinkTap: (url) {
+                print("Opening $url...");
+              },
+              onImageTap: (src) {
+                print(src);
+              },
+              onImageError: (exception, stackTrace) {
+                print(exception);
+              },
+            ),
+          ],
         ),
       ),
     );
